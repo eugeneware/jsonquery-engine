c506fece37a565fbaf33e03d7a7bebef6bcf0c07
Eugene Ware <eugene@noblesamurai.com>
2013-07-29 23:00:30 +1000

    got a lot of tests passing

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ function jsonqueryEngine() {
     query: query,
     match: jsonquery.match,
     plans: {
-      'property': propertyPlan
+      'property': propertyPlan,
+      'pairs': pairsPlan
     }
   };
 }
@@ -87,6 +88,92 @@ function propertyPlan(idx, prop, op, value, negate) {
       return db.indexes[idx].createIndexStream({
         start: [null, null],
         end: [value, undefined]
+      })
+      .pipe(indexFilterStream());
+
+    case '$mod':
+      return db.indexes[idx].createIndexStream()
+      .pipe(indexFilterStream());
+  }
+
+  return null;
+}
+
+function takeTwo(path) {
+  if (path.length > 2) return path.slice(2);
+  return path;
+}
+
+function pairsPlan(idx, prop, op, value, negate) {
+  var db = this, path;
+  var ops = {
+     $gt: function (a, b) { return a > b; },
+    $gte: function (a, b) { return a >= b; },
+     $lt: function (a, b) { return a < b; },
+    $lte: function (a, b) { return a <= b; },
+    $mod: function (a, b) { return a % b[0] === b[1]; }
+  };
+
+  var negOps = {
+     $gt: '$lte',
+    $gte: '$lt',
+     $lt: '$gte',
+    $lte: '$gt'
+  };
+
+  if (negate) {
+    if (op in negOps) {
+      // transform to negated equivalent
+      op = negOps[op];
+      // don't negate predicate as transform already done
+      negate = !negate;
+    }
+  }
+
+  function indexFilterStream() {
+    return through(function (data) {
+      var val = valfn(data);
+      if (ops[op](val, value) === (!negate)) {
+        this.queue(data);
+      }
+    });
+  }
+
+  if (prop) {
+    path = prop.split('.');
+  }
+
+  switch (op) {
+    case '$eq':
+      if (value instanceof RegExp) {
+        return db.indexes[idx].createIndexStream()
+          .pipe(through(function (data) {
+            var val = valfn(data);
+            if (value.test(val)) {
+              this.queue(data);
+            }
+          }));
+      } else {
+        return db.indexes[idx].createIndexStream({
+          start: takeTwo(path.concat(value)).concat(null),
+          end: takeTwo(path.concat(value)).concat(undefined)
+        });
+      }
+      break;
+
+    case '$gt':
+    case '$gte':
+      return db.indexes[idx].createIndexStream({
+        start: takeTwo(path.concat(value)).concat(null),
+        end: takeTwo(path.concat(undefined)).concat(undefined)
+      })
+      .pipe(indexFilterStream());
+
+    case '$lt':
+    case '$lte':
+      return db.indexes[idx].createIndexStream({
+        start: takeTwo(path.concat(null)).concat(null),
+        end: takeTwo(path.concat(value)).concat(undefined)
       })
       .pipe(indexFilterStream());
 
--- test/level-plan.js
+++ test/level-plan.js
@@ -236,6 +236,210 @@ describe('level-plan', function() {
       });
     }
   });
+
+  it('should be able to do jsonqueries with a pairs index', function(done) {
+    var tests = [
+      {
+           query: { 'name': 'name 42' },
+        expected: { indexHits: 1, dataHits: 1, matchHits: 1 },
+      },
+      {
+           query: { $or: [ { num: 420 }, { name: 'name 42' } ] },
+        expected: { indexHits: 2, dataHits: 1, matchHits: 1 }
+      },
+      {
+        query: { $and: [ { tags: 'tag1' }, { num: { $lt: 100 } } ] },
+        expected: { indexHits: 10, dataHits: 10, matchHits: 10 }
+      },
+      {
+        query: { $and: [ { tagsNoIndex: 'tag1' }, { num: { $lt: 100 } } ] },
+        expected: { indexHits: 10, dataHits: 10, matchHits: 10 }
+      },
+      {
+        query: { tags: 'tag4' },
+        expected: { indexHits: 1, dataHits: 1, matchHits: 1 }
+      },
+      {
+        query: { tagsNoIndex: 'tag4' },
+        expected: { indexHits: 1, dataHits: 1, matchHits: 1 }
+      },
+      {
+           query: { 'numNoIndex': 420 },
+        expected: { indexHits: 1, dataHits: 1, matchHits: 1 }
+      },
+      {
+           query: { 'name': { $ne: 'name 42' } },
+        expected: { indexHits: 0, dataHits: 100, matchHits: 99 },
+      },
+      {
+           query: { 'num': { $gte: 500 } },
+        expected: { indexHits: 50, dataHits: 50, matchHits: 50 }
+      },
+      {
+           query: { $not: { 'num': { $gte: 500 } } },
+        expected: { indexHits: 50, dataHits: 50, matchHits: 50 }
+      },
+      {
+           query: { 'num': { $not: { $gte: 500 } } },
+        expected: { indexHits: 50, dataHits: 50, matchHits: 50 }
+      },
+      {
+           query: { 'num': { $gt: 500 } },
+        expected: { indexHits: 49, dataHits: 49, matchHits: 49 }
+      },
+      {
+           query: { $not: { 'num': { $gt: 500 } } },
+        expected: { indexHits: 51, dataHits: 51, matchHits: 51 }
+      },
+      {
+           query: { 'num': { $not: { $gt: 500 } } },
+        expected: { indexHits: 51, dataHits: 51, matchHits: 51 }
+      },
+      {
+           query: { 'num': { $lt: 500 } },
+        expected: { indexHits: 50, dataHits: 50, matchHits: 50 }
+      },
+      {
+           query: { $not: { 'num': { $lt: 500 } } },
+        expected: { indexHits: 50, dataHits: 50, matchHits: 50 }
+      },
+      {
+           query: { 'num': { $not: { $lt: 500 } } },
+        expected: { indexHits: 50, dataHits: 50, matchHits: 50 }
+      },
+      {
+           query: { 'num': { $lte: 500 } },
+        expected: { indexHits: 51, dataHits: 51, matchHits: 51 }
+      },
+      {
+           query: { $not: { 'num': { $lte: 500 } } },
+        expected: { indexHits: 49, dataHits: 49, matchHits: 49 }
+      },
+      {
+           query: { 'num': { $not: { $lte: 500 } } },
+        expected: { indexHits: 49, dataHits: 49, matchHits: 49 }
+      },
+      {
+           query: { num: 420, name: 'name 42' },
+        expected: { indexHits: 1, dataHits: 1, matchHits: 1 }
+      },
+      {
+           query: { num: { $mod: [200, 0] } },
+        expected: { indexHits: 12, dataHits: 6, matchHits: 5 }
+      },
+    /*
+      {
+           query: { $not: { num: { $mod: [200, 0] } } },
+        expected: { indexHits: 95, dataHits: 95, matchHits: 95 }
+      },
+      {
+           query: { num: { $in: [420, 70] } },
+        expected: { indexHits: 2, dataHits: 2, matchHits: 2 }
+      },
+      {
+           query: { num: { $nin: [420, 70] } },
+        expected: { indexHits: 0, dataHits: 100, matchHits: 98 }
+      },
+      {
+           query: { tags: { $all: ['tag2', 'tag4'] } },
+        expected: { indexHits: 1, dataHits: 1, matchHits: 1 }
+      },
+      {
+           query: { tagsNoIndex: { $all: ['tag2', 'tag4'] } },
+        expected: { indexHits: 0, dataHits: 100, matchHits: 1 }
+      },
+      {
+        query: { treeNoIndex: { $elemMatch: { a: 42, b: 43 } } },
+        expected: { indexHits: 0, dataHits: 100, matchHits: 1 }
+      },
+      {
+        query: { tree: { $elemMatch: { a: 42, b: 43 } } },
+        expected: { indexHits: 1, dataHits: 1, matchHits: 1 }
+      },
+      {
+        query: { num: { $or: [ { $gte: 100 }, { $lte: 200 } ] }},
+        expected: { indexHits: 111, dataHits: 100, matchHits: 100 }
+      },
+      {
+        query: { num: { $and: [ { $gte: 100 }, { $lte: 200 } ] }},
+        expected: { indexHits: 11, dataHits: 11, matchHits: 11 }
+      },
+      {
+        query: { $not: { num: { $or: [ { $lt: 100 }, { $gt: 200 } ] } } },
+        expected: { indexHits: 11, dataHits: 11, matchHits: 11 }
+      },
+      {
+        query: { numNoIndex: { $and: [ { $gte: 100 }, { $lte: 200 } ] }},
+        expected: { indexHits: 0, dataHits: 100, matchHits: 11 }
+      },
+      {
+        query: { num: { $not: { $gte: 100 } } },
+        expected: { indexHits: 10, dataHits: 10, matchHits: 10 }
+      },
+      {
+        query: { numNoIndex: { $not: { $gte: 100 } } },
+        expected: { indexHits: 0, dataHits: 100, matchHits: 10 }
+      },
+      {
+        query: { 'tree.a': 42 },
+        expected: { indexHits: 1, dataHits: 1, matchHits: 1 }
+      },
+      {
+        query: { 'tree.b': 43 },
+        expected: { indexHits: 0, dataHits: 100, matchHits: 1 }
+      },
+      {
+           query: { 'name': /^name 42$/ },
+        expected: { indexHits: 1, dataHits: 1, matchHits: 1 }
+      },
+      {
+           query: { 'name': /^name 4/ },
+        expected: { indexHits: 11, dataHits: 11, matchHits: 11 }
+      },
+      */
+    ];
+
+    db = levelQuery(db);
+
+    db.query.use(jsonqueryEngine());
+    db.ensureIndex('*', 'pairs', pairs.index);
+
+    db.batch(testData(), doQueries);
+
+    function doQueries(err) {
+      if (err) return done(err);
+      var candidateTests = tests.filter(function (test) {
+        return test.debug === true || test.only === true;
+      });
+      if (candidateTests.length) tests = candidateTests;
+      var next = after(tests.length, done);
+
+      tests.forEach(function (test) {
+        if (test.before) {
+          test.before(doWork);
+        } else {
+          doWork();
+        }
+
+        function doWork() {
+          var hits = 0;
+          db.query(test.query)
+            .on('data', function (data) {
+              if (test.debug) console.error(data);
+              hits++;
+            })
+            .on('stats', function (stats) {
+              if (test.debug) console.error(stats);
+              expect(stats).to.deep.equals(test.expected);
+            })
+            .on('end', function () {
+              expect(hits).to.equal(test.expected.matchHits);
+              next();
+            });
+        }
+      });
+    }
+  });
 });
 
 function testData() {
